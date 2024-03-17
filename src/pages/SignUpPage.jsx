5416922b404b0e4afa6c7eb48eb5b59f9d50f14c
mridulSehgal <mridul.sehgalsnp@gmail.com>
2024-03-17 20:04:53 +0530

    Register page done, form validation done

--- src/pages/SignUpPage.jsx
+++ src/pages/SignUpPage.jsx
@@ -1,69 +1,90 @@
-/* eslint-disable prettier/prettier */
-import {View, Text, StyleSheet} from 'react-native';
-import React, {useState} from 'react';
-import {Button, HelperText, TextInput, TextInputMask} from 'react-native-paper';
+import { View, Text, StyleSheet, Image, SafeAreaView, ScrollView } from 'react-native'
+import React, { useEffect, useState } from 'react'
+import { Button, HelperText, TextInput, TextInputMask } from 'react-native-paper';
 import DatePicker from 'react-native-date-picker';
 import PhoneInput from 'react-native-phone-number-input';
 import CustomPhoneInput from '../components/CustomPhoneInput';
-import {Controller, useForm} from 'react-hook-form';
+import { Controller, useForm } from 'react-hook-form';
+
 
 const styles = StyleSheet.create({
-  box: {
-    flex: 1,
-    justifyContent: 'center',
-    margin: 10,
-  },
-  headingContainer: {
-    alignItems: 'center',
-  },
-  heading: {
-    color: '#213190',
-    fontSize: 30,
-  },
-  container: {
-    margin: 10,
-    padding: 5,
-  },
-  subHeading: {
-    alignItems: 'flex-end',
-    marginRight: 10,
-  },
-  createAccount: {
-    color: '#213190',
-  },
-  logo: {
-    width: 80,
-    borderRadius: 100,
-    height: 80,
-    marginBottom: 30,
-  },
-});
+    box: {
+        flex: 1,
+        justifyContent: 'center',
 
 
-export const CustomInput = ({control,name,validationRules,placeholder,label, errors})=>{
+    },
+    headingContainer: {
+        // marginTop: -100,
+        alignItems: 'center'
+
+    },
+    heading: {
+        marginTop:5,
+        marginBottom:5,
+        color: '#213190',
+        fontSize: 25,
+
+
+    },
+    container: {
+        margin: 10,
+        padding: 5
+    },
+    subHeading: {
+        alignItems: 'flex-end',
+        marginRight: 10
+
+    },
+    createAccount: {
+        color: '#213190'
+
+    },
+    logo: {
+        width: 60,
+        borderRadius: 50,
+        height: 60,
+        marginTop: 20,
+        objectFit: 'cover'
+
+
+    },
+
+})
+
+
+export const CustomInput = ({control={},name="",validationRules={},placeholder="",label="", errors="",multiline=false,keyboardType,setOpenDatePicker,readonly="false"})=>{
+
+ 
+
+   
 
     return (
         <>
-        
-
+    
         <Controller
       control={control}
       rules={validationRules}
       render={({ field: { onChange, onBlur, value } }) => (
           <TextInput
+          readonly={readonly}
           mode='outlined'
-          placeholder="First name"
+          multiline={multiline}
+          keyboardType={keyboardType}
+          placeholder={placeholder}
           onBlur={onBlur}
           onChangeText={onChange}
           value={value}
-          label={label}
-          error={Object.keys(errors).length !==0}
+          theme={{colors: {primary: '#213190'}}}
+        //   label={label}
+          error={!!errors[name] && Object.keys(errors[name])?.length !==0}
+          right={name==="dob" && <TextInput.Icon name="calendar-range"  onPress={() => setOpenDatePicker(true)} />}
         />
       )}
-      name="firstName"
+      name={name}
       />
-     <HelperText type="error" visible={Object.keys(errors).length !==0}>
-        {errors?.firstName?.message}
+     <HelperText type="error" visible={!!errors[name] && Object.keys(errors[name])?.length !==0}>
+        {errors[name]?.message}
       </HelperText>
       </>
            
@@ -72,55 +93,150 @@ export const CustomInput = ({control,name,validationRules,placeholder,label, err
   
 
 
-
-
-
 const SignUpPage = () => {
 
     const {
         control,
         handleSubmit,
         formState: { errors },
+        setValue,
+        reset,
+        clearErrors
       } = useForm({
         defaultValues: {
           firstName: "",
           lastName: "",
+          address:"",
+          phoneNumber:"",
+          ward:""
         },
       })
-      const onSubmit = (data) => console.log(data)
+      const onSubmit = (data) => {
+        console.log(data)
+        reset();
+      }
 
-return (
-    <View style={styles.box}>
+      const [date, setDate] = useState(new Date())
 
-        <CustomInput
+      const [openDatePicker,setOpenDatePicker] = useState(false);
+   
+
+   
+
+   
+
+    return (
+
+        <>
+          <View style={styles.headingContainer}>
+              
+              <Image style={styles.logo} source={require('../images/aksLogo.png')} />
+
+              <Text style={styles.heading} >Register</Text>
+          </View>
+      
+            <ScrollView>
+        <SafeAreaView>
+                <View style={styles.container} >
+
+
+
+            <CustomInput
         control={control}
-        name={"firstName"}
-        validationRules={{required:{value:true,message:"First name is required"},minLength:{value:3,message:"Minimum of 3 characters required"}}}
+        name="firstName"
+        validationRules={{required:{value:true,message:"First name is required"}}}
         placeholder={"First Name"}
-        label={"First Name"}
+        // label={"First Name"}
         errors={errors}
         />
 
         <CustomInput
         control={control}
-        name={"firstName"}
-        validationRules={{required:{value:true,message:"First name is required"},minLength:{value:3,message:"Minimum of 3 characters required"}}}
-        placeholder={"First Name"}
-        label={"First Name"}
+        name="lastName"
+        validationRules={{required:{value:true,message:"Last name is required"}}}
+        placeholder={"Last Name"}
+        // label={"Last Name"}
         errors={errors}
         />
-   
 
-   
+        <CustomInput
+        control={control}
+        name="address"
+        validationRules={{required:{value:true,message:"Address is required"}}}
+        placeholder={"Address"}
+        // label={"Address"}
+        errors={errors}
+        multiline={true}
+       
 
-    <Button  onPress={handleSubmit(onSubmit)} >Submit</Button>
-  </View>
-)
+        />
+
+        <CustomInput
+        control={control}
+        name="phoneNumber"
+        validationRules={{required:{value:true,message:"Phone number is required"},maxLength:{value:10,message:"Please enter a valid 10 digit phone number"},minLength:{value:10,message:"Please enter a valid 10 digit phone number"},pattern:{value:/^[789]\d{9}$/,message:"Invalid phone number"
+    }}}
+        placeholder={"Phone Number"}
+        // label={"Phone Number"}
+        errors={errors}
+        keyboardType="numeric"
+        />
+
+        <CustomInput
+        control={control}
+        name="ward"
+        validationRules={{required:{value:true,message:"Ward name is required"}}}
+        placeholder={"Ward"}
+        // label={"Ward"}
+        errors={errors}
+        />
+
+        <CustomInput
+        control={control}
+        name="dob"
+        validationRules={{required:{value:true,message:"Date of birth is required"},pattern:{value:/^\d{4}-\d{2}-\d{2}$/,message:"Date of birth should of format YYYY-MM-DD"}}}
+        placeholder={"Date of birth"}
+        // label={"Date of birth"}
+        errors={errors}
+        setOpenDatePicker={setOpenDatePicker}
+        readonly={true}
+        />
+
+<Button loading={false} style={{ marginTop: 5, padding: 2}} mode='contained' color='#213190' onPress={handleSubmit(onSubmit)}  >Submit </Button>
+              
+</View>
+               
+                <DatePicker
+                    theme='light'
+                    mode='date'
+                    modal
+                    open={openDatePicker}
+                    date={date}
+                    onConfirm={(date) => {
+                        setOpenDatePicker(false)
+                        setDate(date)
+                        // const formattedDate = date.getDate()+"-"+date.getMonth()+"-"+date.getFullYear()
+                        setValue("dob",date.toISOString().split('T')[0])
+                        clearErrors("dob")
+                        
+                    }}
+                    onCancel={() => {
+                        setOpenDatePicker(false)
+                    }}
+
+                />  
 
 
 
+               
+               
+        
+           
+     
+                        </SafeAreaView>
+                        </ScrollView>
+                        </>
+    )
 }
 
-    
-    
-export default SignUpPage;
+export default SignUpPage
